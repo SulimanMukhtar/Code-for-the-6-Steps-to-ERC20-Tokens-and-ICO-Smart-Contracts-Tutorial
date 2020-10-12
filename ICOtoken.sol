2a02cbccf1bf9e488d9a90a7ba052407288c6f8a
SulimanMukhtar <SulimanMokhtar1995@gmail.com>
2020-10-12 20:47:07 +0200

    Update ICOtoken.sol

--- ICOtoken.sol
+++ ICOtoken.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.23;
 
 library SafeMath {
     /**
-     * @dev Multiplies two numbers, throws on overflow.
-     **/
+     * g @dev Multiplies two numbers, throws on overflow.
+     **/ 
     function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
         if (a == 0) {
             return 0;
